f6dc29163a3fc5db0993478672f42bfcd7cf28f0
Vlad Kudrashov <39455454+Aironlol@users.noreply.github.com>
2022-11-01 11:39:18 +0300

    new brach

---
+++ homeworks/oop/src/com/oop/PrincessStorage.java
@@ -0,0 +1,12 @@
+package com.oop;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public interface PrincessStorage {
+
+}
